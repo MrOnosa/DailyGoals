2beefc5822e18d322dac8531d8e8e7714193376c
Tyler Palesano <supersnapple@gmail.com>
2015-12-24 19:43:42 -0600

    Real initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,40 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('DailiesCtrl', function($scope, Goals) {
+	$scope.goals = Goals.all();
+
+	$scope.remove = function(goal) {
+    Goals.remove(goal);
+  };
+
+	$scope.add = function(){
+		Goals.add({title: "New goal "});
+	};
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
